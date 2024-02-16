f9e02d69eb8f031725e4fc531d6c945eb4ce2955
Beau <beau@lioncode.nl>
2024-02-16 09:14:23 +0100

    initial setup

---
+++ Source/CrystalCavern/CrystalCavern.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CrystalCavern.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CrystalCavern, "CrystalCavern" );
